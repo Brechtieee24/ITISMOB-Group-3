d3d80a984a7820001dfe64e3b652782c68380fad
Josh Denzel ng <retroduck385@gmail.com>
2025-11-18 00:06:50 +0800

    Removed the action bar

--- app/src/main/java/com/mobdeve/s16/group3/albrechtgabriel/lovelink/ViewMembersActivity.kt
+++ app/src/main/java/com/mobdeve/s16/group3/albrechtgabriel/lovelink/ViewMembersActivity.kt
@@ -2,12 +2,26 @@ package com.mobdeve.s16.group3.albrechtgabriel.lovelink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.View
+import com.mobdeve.s16.group3.albrechtgabriel.lovelink.databinding.ViewMembersPageBinding
 
 class ViewMembersActivity : AppCompatActivity() {
+    private lateinit var binding: ViewMembersPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ViewMembersPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.view_members_page)
+        binding.navbar.navBarContainerLnr.visibility = View.GONE
+
+        // show/hide nav bar options
+        binding.navbar.menuIconNavImgbtn.setOnClickListener {
+            val menuSection = binding.navbar.navBarContainerLnr
+            menuSection.visibility =
+                if (menuSection.visibility == View.VISIBLE) {
+                    View.GONE
+                } else View.VISIBLE
+        }
     }
 }
